32f9645d94de1f568309cf6297f44e913ffe19f6
mjjin967 <myungjin_lee@brown.edu>
2018-01-27 10:09:08 -0500

    signup

--- app/src/main/java/com/jenniferhsia/clothes_minded/MainActivity.java
+++ app/src/main/java/com/jenniferhsia/clothes_minded/MainActivity.java
@@ -2,6 +2,8 @@ package com.jenniferhsia.clothes_minded;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void navigateToSignUp(View v) {
+        Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+        startActivity(intent);
+
+    }
+
+
 }
