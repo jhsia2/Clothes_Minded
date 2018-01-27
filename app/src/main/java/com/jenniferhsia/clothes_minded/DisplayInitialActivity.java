297e1c3515df310818b09ec84da8e6e2f77d811f
mjjin967 <myungjin_lee@brown.edu>
2018-01-27 13:30:10 -0500

    signup, login

---
+++ app/src/main/java/com/jenniferhsia/clothes_minded/DisplayInitialActivity.java
@@ -0,0 +1,13 @@
+package com.jenniferhsia.clothes_minded;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayInitialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_initial);
+    }
+}
